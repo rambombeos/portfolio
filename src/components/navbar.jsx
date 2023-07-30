44960d93c0359bf45645d0ba3afd93f23dacedaa
rambs <rambombeos@gmail.com>
2023-07-30 18:04:54 +0800

    Adding navbar component

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const navbar = () => {
+    return (
+        <>
+            <div className="navbar">
+                <h1>navbars</h1>
+            </div>
+        </>
+    )
+}
+
+export default navbar
